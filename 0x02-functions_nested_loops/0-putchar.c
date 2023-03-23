19a71f40df6ddf706934e7fb7371213c79a8ae19
jaypee15 <okoyejohnpaul15@gmail.com>
2023-03-23 14:54:45 -0700

    added function prototypes to main.h file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#iclude "holberton.h"
+/**
+ * main - entry point for function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}

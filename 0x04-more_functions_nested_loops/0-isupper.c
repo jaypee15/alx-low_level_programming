7ce1f9f30ca259feec4c78fea3e114d79ac50a86
jaypee15 <okoyejohnpaul15@gmail.com>
2023-03-27 02:43:53 -0700

    function to print intergers using putchar

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _isupper check if ch is uppercase
+ * _isupper - check if ch is uppercase
  *
- * @c: character integer to check
- * Return 1 if c is uppercase 
- * return 0 otherwise
+ * @c: (ch) integer to check
+ *
+ * Return: 1 if c is upper, 0 otherwise
  */
 int _isupper(int c)
-{	if (c >= 65 && c <=90)
+{
+	if (c >= 65 && c <= 90)
 		return (1);
 	return (0);
 }
